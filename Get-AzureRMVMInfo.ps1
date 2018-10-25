00492d7c5a567651a2e797b512235414a897813d
martensnico <32136609+martensnico@users.noreply.github.com>
2018-10-25 09:48:48 +0200

    Added cmdlet help

--- Get-AzureRMVMInfo.ps1
+++ Get-AzureRMVMInfo.ps1
@@ -1,3 +1,22 @@
+<#
+    .SYNOPSIS 
+    Gets basic information on one or more Azure VMs.
+    
+    .DESCRIPTION
+    Get CPU, memory and IP configuration with a simple cmdlet.
+    Pipeline input accepts multiple vm objects.
+    .EXAMPLE
+    Get-AzureRMVMInfo -VMName "VM1" -ResourceGroupName "RG1"
+    This command gets the CPU, memory and internal IP for VM "VM1".
+    .EXAMPLE
+    Get-AzureRMVM | Get-AzureRMVMInfo
+    This command gets all VMs in the current subscription, then uses the output to surface all basic configuration on these VMs.
+    .EXAMPLE
+    Get-AzureRMVM | Get-AzureRMVMInfo | Sort "Internal IP"
+    This command gets all VMs in the current subscription, then uses the output to surface all basic configuration on these VMs, sorted by Internal IP.
+    .LINK
+    https://github.com/martensnico/Azure.Helperscripts
+#>
 function Get-AzureRMVMInfo {
     [cmdletbinding(DefaultParameterSetName = 'Name')]
     Param(        
